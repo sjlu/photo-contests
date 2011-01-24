fb73d721b74f0da0a68a3a44b160c6f017d1631b
Steven Lu <tacticalazn@gmail.com>
2011-01-24 03:39:15 +0000

    * Ready for Discovery's valentine promotion

--- details.php
+++ details.php
@@ -1,27 +1,11 @@
 <div style="font-size: 22px; font-weight: bold; margin-bottom: 8px;">Details</div>
 
 <div style="margin-left: 15px; font-size: 14px">
-<b>ENTER NOW</b> to win tickets to Lady Gaga's Monster Ball!<br />
-<br />
-<b>Show us what YOU'D wear</b> to the Monster Ball!<br />
-<br />
-<b>Entering is easy!</b> GO GLAM for Gaga! Put together your best concert-going look, take a photo, and upload it right here!<br />
-<br />
-<b>Write a short description</b> about your outfit. Tell us what it is and why you love it. Personality and creativity in your description counts!<br />
-<br />
-<b>A pro panel of Discovery-appointed fashion professionals</b> will act as judges and choose the top 5 pics based on style, overall display, creativity of description and personality. Have tons of fun and be yourself!<br />
-<br />
-<b>WINNERS will be announced</b> on Facebook on Feb. 1, 2011.<br />
-<br />
-<b>Grand Prize Winner</b> will receive 4 tickets to see Lady Gaga on Feb. 28, plus a free parking pass & $500 Discovery Gift Card!<br />
-<br />
-<b>4 Runner Up Winners</b> will receive gift cards!<br />
-<blockquote>
-   <li>1st place: $250</li>
-   <li>2nd place: $150</li>
-   <li>3rd place: $100</li>
-   <li>4th place: $50</li>
-</blockquote>
+<?php
+$fh = fopen("details.txt", 'r');
+$pageText = fread($fh, 25000);
+echo nl2br($pageText);
+?>
 </div>
 
 
@@ -29,5 +13,9 @@
 <a clicktotoggle="rules">View Official Rules & Privacy Policy</a>
 </div>
 <div id="rules" style="margin-left: 15px; font-size: 12px; <?php if(!isset($_GET['rules'])) echo 'display:none;'; ?>">
-<?php include_once('rules.txt'); ?>
+<?php
+$fh = fopen("rules.txt", 'r');
+$pageText = fread($fh, 25000);
+echo nl2br($pageText);
+?>
 </div>
