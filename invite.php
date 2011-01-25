9f88b304598032f1b75b3baedf6b6a6b87479758
Steven Lu <tacticalazn@gmail.com>
2011-01-25 17:23:42 +0000

    * another update to discoveryapp-val

--- invite.php
+++ invite.php
@@ -3,7 +3,7 @@
 method="post"
 type="Discovery Clothing"
 invite="true"
-content="Your friend wants you to check this contest out! <fb:req-choice url='http://apps.facebook.com/discoveryapp/' label='GO' /> ">
+content="Your friend wants you to check this contest out! <fb:req-choice url='http://apps.facebook.com/discoveryapp-val/' label='GO' /> ">
 <fb:multi-friend-selector actiontext="Share this page with your friends" rows="3" showborder="false" cols="5" />
 </fb:request-form>
 </center>
